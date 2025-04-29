b35b6415db6e9ff5db3fc48803e9b920bf322afe
Hugo Stackhouse <hugo.stackhouse@sydney.edu.au>
2025-04-29 15:38:41 +1000

    Allow params to specify a grid to search upon

--- test/meta_hyperopt.jl
+++ test/meta_hyperopt.jl
@@ -1,9 +1,9 @@
-# using Revise
+using Revise
 using MPSTime
 using JLD2
 using Distributed
 # using Optimization
-# using OptimizationBBO
+using OptimizationBBO
 using Random
 # using OptimizationMetaheuristics
 # using OptimizationOptimJL
@@ -14,53 +14,55 @@ Random.seed!(1)
 @load "test/Data/italypower/datasets/ItalyPowerDemandOrig.jld2" X_train y_train X_test y_test
 
 params = (
-    eta=(-3,log10(0.5)), 
-    d=(5,15), 
-    chi_max=(20,40),
+    eta=logrange(10^-3,0.5; length=10), 
+    d=[2,5,7,8,15],#5,15), 
+    chi_max=(5,5,10)#20,40),
 ) 
+nfolds = 2
+
 e = copy(ENV)
 e["OMP_NUM_THREADS"] = "1"
 e["JULIA_NUM_THREADS"] = "1"
 
 if nprocs() == 1
-    addprocs(10; env=e, exeflags="--heap-size-hint=2G", enable_threaded_blas=false)
+    addprocs(2; env=e, exeflags="--heap-size-hint=2G", enable_threaded_blas=false)
 end
 
-@everywhere using MPSTime, Distributed, Optimization
+@everywhere using MPSTime, Distributed, Optimization, OptimizationBBO
 
 rs_f = jldopen("Folds/IPD/ipd_resample_folds_julia_idx.jld2", "r");
 fold_idxs = read(rs_f, "rs_folds_julia");
 close(rs_f)
 
 @load "Folds/IPD/ipd_windows_julia_idx.jld2" windows_julia
-folds = [(fold_idxs[i-1]["train"], fold_idxs[i-1]["test"]) for i in 1:30]
+folds = [(fold_idxs[i-1]["train"], fold_idxs[i-1]["test"][1:10]) for i in 1:30]
 
 Xs = vcat(X_train, X_test)
 ys = zeros(Int, size(Xs, 1))
 res = evaluate(
     Xs,
     ys,
-    2,
+    nfolds,
     params,
     MPSRandomSearch(); 
     objective=ImputationLoss(), 
     opts0=MPSOptions(; verbosity=-5, log_level=-1, nsweeps=10, sigmoid_transform=false), 
-    n_cvfolds=5,
+    n_cvfolds=2,
     eval_windows=windows_julia,
     eval_pms=nothing,#collect(5:20:95) ./100,
     tuning_windows = nothing,
     tuning_pms=collect(5:10:95) ./100,
     tuning_abstol=1e-9, 
-    tuning_maxiters=14,
+    tuning_maxiters=11,
     verbosity=2,
     foldmethod=folds,
     input_supertype=Float64,
     provide_x0=false,
-    logspace_eta=true,
+    # logspace_eta=true,
     distribute_folds=true,
     distribute_cvfolds=false,
-    distribute_iters=true,
-    distribute_final_eval=true,
+    distribute_iters=false,
+    distribute_final_eval=false,
     write=false,
     writedir="IPD_final"
 )
